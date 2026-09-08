7d21d1bde175475317239508fa70c086a86651f8
mir-j-ru <vania200005k@gmail.com>
2026-09-08 08:02:43 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,67 @@
+import telebot
+import requests
+from datetime import datetime
+from telebot import types
+from scheduler import start_scheduler
+
+TOKEN = "8652981939:AAGriJZvqrfiI73bFWLOKb5vUcSlHfxM8nc"
+bot = telebot.TeleBot(TOKEN)
+SERVER_URL = "http://127.0.0.1:8080"
+ADMIN_ID = 6154565499
+
+
+@bot.message_handler(commands=['start'])  # стартовая команда
+def start(message):
+    if message.chat.id == ADMIN_ID:
+        markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+        btn1 = types.KeyboardButton('➕ добавить копмлектацию')
+        markup.add(btn1)
+
+        bot.reply_to(message, "🛠 Админ-панель:", reply_markup=markup)
+        return
+
+user_data = {}
+
+@bot.message_handler(func=lambda message: message.text == '➕ добавить слот')
+def start_slot(message):
+    chat_id = message.chat.id
+    if chat_id != ADMIN_ID:
+        bot.reply_to(message, "вы не являетесь администратором")
+        return
+    user_data[message.chat.id] = {}
+    bot.reply_to(message, "📝 Введите время начала:")
+    bot.register_next_step_handler(message, get_master_name_slot)
+
+def get_master_name_slot(message):
+    chat_id = message.chat.id
+    user_data[chat_id]['start_time'] = message.text if message.text != '-' else 'Не указан'
+    bot.reply_to(message, "введите время окончания:")
+    bot.register_next_step_handler(message, get_review_text_slot)
+
+def get_review_text_slot(message):
+    chat_id = message.chat.id
+    user_data[chat_id]['end_time'] = message.text
+    bot.reply_to(message, "введите статус:")
+    bot.register_next_step_handler(message, get_rating_slot)
+
+def get_rating_slot(message):
+    chat_id = message.chat.id
+
+    user_data[chat_id]['status'] = message.text
+
+    # Теперь у нас есть все данные
+    data = user_data[chat_id]
+
+
+    response = requests.get(f"https://server-for-mir-j-production.up.railway.app/get_waregouse?name={user_data[chat_id]['start_time']}&quantity=10&category={user_data[chat_id]['end_time']}&number={user_data[chat_id]['status']}")
+    if response.status_code == 200:
+        bot.reply_to(message,
+                     f"время добавлено!\n🕟начало - {data['start_time']}\n🕟конец - {data['end_time']}\nстатус - {data['status']}\n")
+    else:
+        bot.reply_to(message, "ошибка сервера")
+        return
+
+    del user_data[chat_id]  # Очищаем данные пользователя
+
+if __name__ == '__main__':
+    bot.infinity_polling()
